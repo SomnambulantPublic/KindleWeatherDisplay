7f0847b759807f3597998d5f65965d67a3d5d9d9
Chris Lovell <somnambulantpublic@gmail.com>
2018-05-15 18:35:18 +1000

    ver 0.8

--- server/weather_script.py
+++ server/weather_script.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-
+#
 # Kindle Weather Display - Server-side script
 # Chris Lovell, April 2018
-
+#
+# Revison 0.8
+# - Added revison note
+# - Improved comments
+# - Descript text wrapping bug-fix (will now cut long sentences more than just once)
+#
+# Inspired by the work of:
+# Matthew Petroff (http://www.mpetroff.net/) September 2012
+#
 # Updated for python3;
 # Parsing rewritten for Australian Government Bureau of Meteorology (BOM) data;
 # Additional data elements added to output
-
-# Based on the work of:
-# Matthew Petroff (http://www.mpetroff.net/) September 2012
-
-#### Notes: ####
 #
-# Commented-out code is for debug purposes
+#### Notes: ####
 #
 # BOM product descriptions: http://www.bom.gov.au/catalogue/anon-ftp.shtml
 # Hosted: (browser) ftp://ftp.bom.gov.au/anon/gen/ or (client) ftp://ftp2.bom.gov.au
@@ -24,7 +27,6 @@
 from ftplib import FTP
 from xml.dom import minidom
 from datetime import datetime, timedelta
-
 
 # Constants
 dates = 0
@@ -52,15 +54,16 @@ forecast_ID = None
 img_ID = datetime.now()
 
 # Arrays for data 
-# precis forecast (8 days with 6 data points)
+# precis (short) forecast (8 days with 6 data points)
 forecast = [["" for y in range(6)] for x in range(8)]
-# descriptive forecast (8 days)
+# long descriptive forecast (8 days)
 descript = ["" for z in range(8)]
 # current conditions
 observations = ["" for v in range(6)]
 
 
-# Download weather forecast
+# Download and save weather forecast
+#
 bom = FTP('ftp2.bom.gov.au', user='anonymous', passwd='guest')
 bom.cwd('/anon/gen/fwo')
 precis_xml = open('IDN11052.xml', 'wt')
@@ -70,6 +73,7 @@ bom.quit()
 
 
 # Parse weather data incl issue date time, forecast dates, precis descriptions, icons
+#
 precis_xml = open('IDN11052.xml', 'rt')
 precis_data = minidom.parse(precis_xml)
 precis_areas = precis_data.getElementsByTagName('area')
@@ -84,61 +88,50 @@ for area in precis_areas:
             forecast[pi][dates] = period.getAttribute('start-time-local')
             elements = period.getElementsByTagName('*') #*1 See Notes
             for element in elements:
-                #print ("Period:" + str(pi))
                 if element.getAttribute('type') == 'forecast_icon_code':
                     forecast[pi][icons] = str(element.firstChild.nodeValue)
-                    #print ("Icon " + str(element.firstChild.nodeValue))
-                    #print ("X: " + str(pi) + " , Y: " + str(icons))
                 elif element.getAttribute('type') == 'air_temperature_minimum':
                     forecast[pi][lows] = str(element.firstChild.nodeValue)
-                    #print ("Lows " + str(element.firstChild.nodeValue))
-                    #print ("X: " + str(pi) + " , Y: " + str(lows))
                 elif element.getAttribute('type') == 'air_temperature_maximum':
                     forecast[pi][highs] = str(element.firstChild.nodeValue)
-                    #print ("Highs " + str(element.firstChild.nodeValue))
-                    #print ("X: " + str(pi) + " , Y: " + str(highs))
                 elif element.getAttribute('type') == 'precis':
                     forecast[pi][briefs] = str(element.firstChild.nodeValue)
-                    #print("Briefs " + str(element.firstChild.nodeValue))
-                    #print ("X: " + str(pi) + " , Y: " + str(briefs))
                 elif element.getAttribute('type') == 'probability_of_precipitation':
                     forecast[pi][rains] = str(element.firstChild.nodeValue)
-                    #print("Rains " + str(element.firstChild.nodeValue))
-                    #print ("X: " + str(pi) + " , Y: " + str(rains))
-            #print (forecast[pi])
-            #print (forecast)
-    # Get all descriptive forecasts
+    # Get ALL long descriptive forecasts
     if area.getAttribute('description') == 'Central Coast':
         periods = area.getElementsByTagName('forecast-period')
         for period in periods:
             pi = periods.index(period)
             elements = period.getElementsByTagName('*') #*1 See Notes
             for element in elements:
-                #print ("Period:" + str(pi))
                 if element.getAttribute('type') == 'forecast':
                     descript[pi] = str(element.firstChild.nodeValue) 
 precis_xml.close()
 
-#for i in range(len(forecast)):
-#    print ("i: " +str(i))
-#    for j in range(len(forecast[i])):
-#        print ("j: "+str(j))
-#        print(forecast[i][j])
-#    print(forecast[i])
-
 
 # Split long forecast (for today only) into multiple lines, also rudimentary text-wrap, and format for SVG text
+#
+# for the number of sentences in the first long description
 for w in range(len(descript[0].split('. '))):
+    # take the whole description, remove last 'period', split on 'period-space', return each one sentence (eg 1/2/3) for each iteration
     split_descript = str(descript[0][:-1].split('. ')[w])
-    if len(split_descript) > 55:
+    # if sentence is greater than 55 chatacters
+    while len(split_descript) > 55:
+        # find the first space after character 44
         n = split_descript.index(' ', 45)
-        parse_descript = parse_descript + """<tspan dy="22" x="300">""" + split_descript[:n] + "</tspan>" + """<tspan dy="22" x="300">""" + split_descript[(n+1):] + "</tspan>"
+        # split at that space, put text up to that point into the 'parse_descript' string
+        parse_descript = parse_descript + """<tspan dy="22" x="300">""" + split_descript[:n] + "</tspan>"
+        # put all text after the space into next while-loop (omitting the space itself)
+        split_descript = str(split_descript[(n+1):])
     else:
+        # put the text into 'parse_descript' string
         parse_descript = parse_descript + """<tspan dy="22" x="300">""" + split_descript + "</tspan>"
         
 # Split precis for rudimentary text-wrap, and format for SVG text
+#
 for u in range(len(forecast)):
-    # List containing x coordinates for displaying precis data
+    # List containing x coordinates for displaying precis data (only interested in data for days 1-3)
     k = [0,100,300,500,0,0,0,0]
     # Remove trailing period '.'    
     precis = forecast[u][briefs][:-1]
@@ -150,6 +143,7 @@ for u in range(len(forecast)):
         forecast[u][briefs] = '<tspan dy="17" x="' + str(k[u]) + '">' + precis + "</tspan>"    
 
 # Download observations
+#
 bom = FTP('ftp2.bom.gov.au', user='anonymous', passwd='guest')
 bom.cwd('/anon/gen/fwo')
 obs_xml = open('IDN60920.xml', 'wt')
@@ -158,6 +152,7 @@ obs_xml.close()
 bom.quit()
 
 # Parse weather observations
+#
 obs_xml = open('IDN60920.xml', 'rt')
 obs_data = minidom.parse(obs_xml)
 obs_ID = datetime.strptime(obs_data.getElementsByTagName('issue-time-local')[0].firstChild.nodeValue, '%Y-%m-%dT%H:%M:%S+10:00')
@@ -182,48 +177,35 @@ for station in obs_stations:
 obs_xml.close()
 
 
-# TODO consider UV
-# TODO consider sunrise and sunset
-
-
-
-# Load from preprocess SVG and substitute values
-
+# Load from preprocess SVG, substitute values, write output
+#
 # Open SVG to process
 output = open('weather-script-preprocess.svg', 'r', encoding='utf-8').read()
-
 # Insert icons
 output = output.replace('ICON_ZERO',str("ico"+forecast[0][icons])).replace('ICON_ONE',str("ico"+forecast[1][icons])).replace('ICON_TWO',str("ico"+forecast[2][icons])).replace('ICON_THREE',str("ico"+forecast[3][icons]))
-
 # Insert temperatures
 output = output.replace('HIGH_ZERO',forecast[0][highs]).replace('HIGH_ONE',forecast[1][highs]).replace('HIGH_TWO',forecast[2][highs]).replace('HIGH_THREE',forecast[3][highs])
 output = output.replace('LOW_ZERO',forecast[0][lows]).replace('LOW_ONE',forecast[1][lows]).replace('LOW_TWO',forecast[2][lows]).replace('LOW_THREE',forecast[3][lows])
-
 # Insert precis
 output = output.replace('PRECIS_ONE',forecast[1][briefs]).replace('PRECIS_TWO',forecast[2][briefs]).replace('PRECIS_THREE',forecast[3][briefs])
-
 # Insert descript
 output = output.replace('DESCRIPT_ZERO',parse_descript)
-
 # Insert days of week words
 one_day = timedelta(days=1)
 days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 output = output.replace('DAY_TWO',days_of_week[(xml_day_today + 2*one_day).weekday()]).replace('DAY_THREE',days_of_week[(xml_day_today + 3*one_day).weekday()])
-
 # Insert current observations
 # feels like, actual temp, humidity, 
 output = output.replace('FEELS_NOW',observations[apparent]).replace('TEMP_NOW',observations[actual]).replace('HUMID_NOW',observations[humid])
-
-# Insert wind direction, pointer visibility, speedkmh, speedkts
+# wind direction, pointer visibility, speedkmh, speedkts
 output = output.replace('WIND_DEG',str(observations[winddir]+180)).replace('WIND_KTS',observations[windkts]).replace('WIND_KMH',str(observations[windkmh]))
+# if wind speed is zero, hide wind-vane pointer
 if observations[windkmh] > 0:
     output = output.replace('POINTER_VIS','visible')
 else:
     output = output.replace('POINTER_VIS','hidden')
-
 # Insert timestamps
 output = output.replace('IMG_GEN_DT',img_ID.strftime('%Y-%m-%d %H:%M:%S')).replace('FORECAST_DT',forecast_ID.strftime('%Y-%m-%d %H:%M:%S')).replace('OBS_DT',obs_ID.strftime('%Y-%m-%d %H:%M:%S'))
-
 # Write output
 open('weather-script-output.svg', 'w', encoding='utf-8').write(output)
 
