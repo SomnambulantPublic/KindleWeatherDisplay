55ba27a077f1d22bdd24902938fa15d231ebd673
Chris Lovell <somnambulantpublic@gmail.com>
2018-05-15 18:26:46 +1000

    ver 0.2

--- server/weather_script.py
+++ server/weather_script.py
@@ -28,13 +28,13 @@ bom.quit()
 #
 # Parse weather data
 #
-# 4 days with 6 data points
-forecast = [[None]*6]*4
+# 8 days with 6 data points
+forecast = [[None]*6]*8
 # constants help readability
 dates = 0
 icons = 1
-highs = 2
-lows = 3
+lows = 2
+highs = 3
 briefs = 4
 rains = 5
 
@@ -44,26 +44,38 @@ precis_areas = precis_data.getElementsByTagName('area')
 for area in precis_areas:
     if area.getAttribute('description') == 'Gosford':
         periods = area.getElementsByTagName('forecast-period')
-        # Get 4 days of forecast data
-        for i in (0-3):
-            forecast[i][dates] = periods[i].getAttribute('start-time-local')
-            for period in periods:
-                elements = period.getElementsByTagName('element')
-                for element in elements:
-                    if element.getAttribute('type') == 'forecast_icon_code':
-                        forecast[i][icons] = int(element.firstChild.nodeValue)
-                    elif element.getAttribute('type') == 'air_temperature_minimum':
-                        forecast[i][lows] = int(element.firstChild.nodeValue)
-                    elif element.getAttribute('type') == 'air_temperature_maximum':
-                        forecast[i][highs] = int(element.firstChild.nodeValue)
-                texts = period.getElementsByTagName('text')
-                for text in texts:
-                    if text.getAttribute('type') == 'precis':
-                        forecast[i][briefs] = element.firstChild.nodeValue
-                    elif text.getAttribute('type') == 'probability_of_precipitation':
-                        forecast[i][rains] = element.firstChild.nodeValue
+        for period in periods:
+            forecast[periods.index(period)][dates] = period.getAttribute('start-time-local')
+            elements = period.getElementsByTagName('*')
+#        
+#            # alternate to get element nodes wildcard above is to get all children
+#            elements = period.childNodes 
+#            # then trim out text nodes, text objects will fail .getAttribute()
+#            # note: I don't mean element nodes with .tagName of text, these are fine
+#            for i in range(elements.length):
+#                if elements[i].nodeType == 3:
+#                    elements.remove(elements[i])
+#        
+            for element in elements:
+                if element.getAttribute('type') == 'forecast_icon_code':
+                    forecast[periods.index(period)][icons] = int(element.firstChild.nodeValue)
+                elif element.getAttribute('type') == 'air_temperature_minimum':
+                    forecast[periods.index(period)][lows] = int(element.firstChild.nodeValue)
+                elif element.getAttribute('type') == 'air_temperature_maximum':
+                    forecast[periods.index(period)][highs] = int(element.firstChild.nodeValue)
+                elif element.getAttribute('type') == 'precis':
+                    forecast[periods.index(period)][briefs] = element.firstChild.nodeValue
+                elif element.getAttribute('type') == 'probability_of_precipitation':
+                    forecast[periods.index(period)][rains] = element.firstChild.nodeValue
+                else:
+                    continue
+                
+for i in range(8):
+    for j in range(6):
+        print(forecast[i][j])
+                        
 ## TODO: try getChildren and nextSibling methods to clean up mess above (from below for period in periods)
 
 
 # Enhance memory recovery
-#dom.unlink()
+#dom.unprintlink()
