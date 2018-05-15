774996b518307923dab555f9bb5160495882a966
Chris Lovell <somnambulantpublic@gmail.com>
2018-05-15 18:34:09 +1000

    ver 0.5

--- server/weather_script.py
+++ server/weather_script.py
@@ -3,8 +3,9 @@
 # Kindle Weather Display - Server-side script
 # Chris Lovell, April 2018
 
-# Updated for python3; and
-# Parsing rewritten for Australian Government Bureau of Meteorology (BOM) data
+# Updated for python3;
+# Parsing rewritten for Australian Government Bureau of Meteorology (BOM) data;
+# Additional data elements added to output
 
 # Based on the work of:
 # Matthew Petroff (http://www.mpetroff.net/) September 2012
@@ -34,28 +35,33 @@ briefs = 4
 rains = 5
 
 # variable
-xml_day_today = None
+xml_date_today = None
 xml_day_today = None
 
-# 'Array' of lists for data (8 days with 6 data points)
+# Arrays for data 
+# precis forecast (8 days with 6 data points)
 forecast = [[None for y in range(6)] for x in range(8)]
+# descriptive forecast (8 days)
+descript = [None for z in range(8)]
+parse_descript = ""
 
-# Download weather data
+# Download weather forecast
 bom = FTP('ftp2.bom.gov.au', user='anonymous', passwd='guest')
 bom.cwd('/anon/gen/fwo')
-precis_xml = open('IDN11060.xml', 'wt')
-bom.retrlines('RETR IDN11060.xml', precis_xml.write)
+precis_xml = open('IDN11052.xml', 'wt')
+bom.retrlines('RETR IDN11052.xml', precis_xml.write)
 precis_xml.close()
 bom.quit()
 
-# Parse weather data incl dates
-precis_xml = open('IDN11060.xml', 'rt')
+# Parse weather data incl icons, dates, descriptions
+precis_xml = open('IDN11052.xml', 'rt')
 precis_data = minidom.parse(precis_xml)
 precis_areas = precis_data.getElementsByTagName('area')
 for area in precis_areas:
     if area.getAttribute('description') == 'Gosford':
         periods = area.getElementsByTagName('forecast-period')
         xml_date_today = periods[0].getAttribute('start-time-local').split('T', 1)[0]
+        # TODO review date parsing format here, re hyphens 
         xml_day_today = datetime.datetime.strptime(xml_date_today, '%Y-%m-%d')
         for period in periods:
             pi = periods.index(period)
@@ -85,7 +91,16 @@ for area in precis_areas:
                     #print ("X: " + str(pi) + " , Y: " + str(rains))
             #print (forecast[pi])
             #print (forecast)
-
+    if area.getAttribute('description') == 'Central Coast':
+        periods = area.getElementsByTagName('forecast-period')
+        for period in periods:
+            pi = periods.index(period)
+            elements = period.getElementsByTagName('*') #*1 See Notes
+            for element in elements:
+                #print ("Period:" + str(pi))
+                if element.getAttribute('type') == 'forecast':
+                    descript[pi] = str(element.firstChild.nodeValue) 
+precis_xml.close()
 
 #for i in range(len(forecast)):
 #    print ("i: " +str(i))
@@ -95,6 +110,37 @@ for area in precis_areas:
 #    print(forecast[i])
 
 
+# TODO: TEST THIS 
+# Split long forecast into multiple lines and format for SVG
+for w in range(len(descript[0].split('.'))):
+    parse_descript = parse_descript + """<tspan dy="23" x="300">""" + str(descript[0].split('.')[w]) + "</tspan>"
+    
+
+# Download observations
+bom = FTP('ftp2.bom.gov.au', user='anonymous', passwd='guest')
+bom.cwd('/anon/gen/fwo')
+obs_xml = open('IDN60920.xml', 'wt')
+bom.retrlines('RETR IDN60920.xml', obs_xml.write)
+obs_xml.close()
+bom.quit()
+
+# Parse weather observations
+obs_xml = open('IDN60920.xml', 'rt')
+obs_data = minidom.parse(obs_xml)
+obs_stations = obs_data.getElementsByTagName('station')
+for station in obs_stations:
+    if station.getAttribute('description') == 'Gosford':
+        elements = station.getElementsByTagName('*')
+        for element in elements:
+            if element.getAttribute('') == '':
+                continue
+    
+
+obs_xml.close()
+
+# TODO consider UV and sunrise and sunset
+
+
 #
 # Preprocess SVG
 #
@@ -102,23 +148,28 @@ for area in precis_areas:
 # Open SVG to process
 output = codecs.open('weather-script-preprocess.svg', 'r', encoding='utf-8').read()
 
-# Insert icons and temperatures
-output = output.replace('ICON_ONE',str("ico"+forecast[0][1])).replace('ICON_TWO',str("ico"+forecast[1][1])).replace('ICON_THREE',str("ico"+forecast[2][1])).replace('ICON_FOUR',str("ico"+forecast[3][1]))
-output = output.replace('HIGH_ONE',str(forecast[0][3])).replace('HIGH_TWO',str(forecast[1][3])).replace('HIGH_THREE',str(forecast[2][3])).replace('HIGH_FOUR',str(forecast[3][3]))
-output = output.replace('LOW_ONE',str(forecast[0][2])).replace('LOW_TWO',str(forecast[1][2])).replace('LOW_THREE',str(forecast[2][2])).replace('LOW_FOUR',str(forecast[3][2]))
+# Insert icons
+output = output.replace('ICON_ZERO',str("ico"+forecast[0][1])).replace('ICON_ONE',str("ico"+forecast[1][1])).replace('ICON_TWO',str("ico"+forecast[2][1])).replace('ICON_THREE',str("ico"+forecast[3][1]))
+
+# Insert temperatures
+output = output.replace('HIGH_ZERO',str(forecast[0][3])).replace('HIGH_ONE',str(forecast[1][3])).replace('HIGH_TWO',str(forecast[2][3])).replace('HIGH_THREE',str(forecast[3][3]))
+output = output.replace('LOW_ZERO',str(forecast[0][2])).replace('LOW_ONE',str(forecast[1][2])).replace('LOW_TWO',str(forecast[2][2])).replace('LOW_THREE',str(forecast[3][2]))
+
+# Insert precis
+output = output.replace('PRECIS_ONE',str(forecast[1][4])).replace('PRECIS_TWO',str(forecast[2][4])).replace('PRECIS_THREE',str(forecast[3][4]))
+
+# Insert descript
+output = output.replace('DESCRIPT_ZERO',parse_descript)
 
 # Insert days of week
 one_day = datetime.timedelta(days=1)
 days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-output = output.replace('DAY_THREE',days_of_week[(xml_day_today + 2*one_day).weekday()]).replace('DAY_FOUR',days_of_week[(xml_day_today + 3*one_day).weekday()])
+output = output.replace('DAY_TWO',days_of_week[(xml_day_today + 2*one_day).weekday()]).replace('DAY_THREE',days_of_week[(xml_day_today + 3*one_day).weekday()])
 
 # Write output
 codecs.open('weather-script-output.svg', 'w', encoding='utf-8').write(output)
 
 
-
-
-
 # Enhance memory recovery
-#precis_data.unlink()
-
+precis_data.unlink()
+obs_data.unlink()
